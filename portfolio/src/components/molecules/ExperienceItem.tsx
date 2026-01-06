e63162f395262933f79f0aeaa0dc90497f839591
FavioGenaro <faviogenarosaico@gmail.com>
2026-01-05 20:22:49 -0500

    feat: Agregar color a line y point experience

--- portfolio/src/components/molecules/ExperienceItem.tsx
+++ portfolio/src/components/molecules/ExperienceItem.tsx
@@ -9,11 +9,11 @@ interface ExperienceProps {
 const ExperienceItem: React.FC<ExperienceProps> = ({ expItem }) => {
   return (
     <div
-        className="relative mx-12 pb-4 before:absolute before:left-[-35px] before:block before:h-[74%] flex items-end before:border-l-2 before:border-black/50 dark:before:border-white/80 before:content-['']"
+        className="relative mx-12 pb-4 before:absolute before:left-[-35px] before:block before:h-[74%] flex items-end lineExperience before:border-l-2 before:content-['']"
     >
         <div className="relative pb-4 dark:text-white">
             <div className="sticky top-0">
-                <span className='line-time absolute -left-[42px] top-[8px] rounded-full w-4 h-4 bg-white'></span>
+                <span className='line-time absolute -left-[42px] top-[8px] rounded-full w-4 h-4 pointExperience'></span>
                 {/* Contenido */}
                 <h3 className="titleH3 font-bold text-blue-900 dark:text-blue-400">{expItem.jobName}</h3>
                 <h4 className="titleH4 font-semibold text-gray-600 dark:text-white">{expItem.companyName}</h4>
